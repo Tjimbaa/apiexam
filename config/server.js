86df556d782de79be2a49a25c71f8883211ef4f4
Tj <thinh_tran_4@msn.com>
2021-05-29 19:57:51 +0200

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b1129ca109ba18aa30929973a6e0eecf'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f91c5ca6-1f53-4930-823e-63459aee341b'
+};
